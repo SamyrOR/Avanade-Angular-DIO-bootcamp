e21f5fa915cdef8a45df64e3887bf4cb94c5129c
Samyr O. Ribeiro <saamyr@live.com>
2021-02-25 22:51:13 -0300

    criado components e form para busca

--- jv-weather-projeto/src/environments/environment.prod.ts
+++ jv-weather-projeto/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: 'b9862ff2acfea82de01e3b9bb12bf70f',
 };
--- jv-weather-projeto/src/environments/environment.ts
+++ jv-weather-projeto/src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: 'b9862ff2acfea82de01e3b9bb12bf70f',
 };
 
 /*
