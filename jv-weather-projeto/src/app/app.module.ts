852893b7b22b4eca3bcbebe92a32d6ae6b85cf67
Samyr O. Ribeiro <saamyr@live.com>
2021-02-27 23:33:23 -0300

    criado toda parte reativa e conexao com a API

--- jv-weather-projeto/src/app/app.module.ts
+++ jv-weather-projeto/src/app/app.module.ts
@@ -5,10 +5,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeModule } from './pages/home/home.module';
 import { BookmarksModule } from './pages/bookmarks/bookmarks.module';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { EffectsModule } from '@ngrx/effects';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HomeModule, BookmarksModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HomeModule,
+    BookmarksModule,
+    StoreModule.forRoot({}),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production,
+    }),
+    EffectsModule.forRoot([]),
+    HttpClientModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
---
+++ jv-weather-projeto/src/app/pages/home/containers/home/home.page.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+
+import { select, Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { Bookmark } from 'src/app/shared/models/bookmark.model';
+import { CityWeather } from 'src/app/shared/models/weather.model';
+
+import * as fromHomeActions from '../../state/home.actions';
+import * as fromHomeSelectors from '../../state/home.selectors';
+
+@Component({
+  selector: 'jv-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+})
+export class HomePage implements OnInit {
+  cityWeather$: Observable<CityWeather>;
+  loading$: Observable<boolean>;
+  error$: Observable<boolean>;
+
+  searchControl: FormControl;
+
+  constructor(private store: Store) {}
+
+  ngOnInit(): void {
+    this.searchControl = new FormControl('', Validators.required);
+
+    this.cityWeather$ = this.store.pipe(
+      select(fromHomeSelectors.selectCurrentWeather)
+    );
+    this.loading$ = this.store.pipe(
+      select(fromHomeSelectors.selectCurrentWeatherLoading)
+    );
+    this.error$ = this.store.pipe(
+      select(fromHomeSelectors.selectCurrentWeatherError)
+    );
+  }
+
+  doSearch() {
+    const query = this.searchControl.value;
+    this.store.dispatch(fromHomeActions.loadCurrentWeather({ query }));
+  }
+
+  onToggleBookmark() {
+    const bookmark = new Bookmark();
+  }
+}
